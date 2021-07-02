b0fd1a688a785ba758a5b00f0b1f90f8722c5317
UesleiFerraz <uesleivaleriod@gmail.com>
2021-07-02 12:06:49 -0300

    inicial setup done

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+module.exports = {
+  type: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false,
+  logging: true,
+  entities: ["./src/core/data/database/entities/**/*"],
+  migrations: ["./src/core/data/database/migrations/**/*"],
+  cli: {
+    entitiesDir: "/src/core/data/database/entities",
+    migrationsDir: "/src/core/data/database/migrations",
+  },
+};
